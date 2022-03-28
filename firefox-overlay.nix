ca42d6b9823c8564a90fdf3e95cb46fccfbada6a
Anders Kaseorg <andersk@mit.edu>
2022-03-28 12:33:07 -0700

    firefoxVersion: Fix for upstream applicationName → binaryName rename
    Fixes this error on current nixos-unstable:
    
    $ nix build -Lf '<nixpkgs>' latest.firefox-bin
    error: 'wrapper' at /nix/store/dmb2chpbx965shwigb1zbr8zwjz34sgj-nixos/nixos/pkgs/applications/networking/browsers/firefox/wrapper.nix:24:5 called with unexpected argument 'browserName'
    
           at /nix/store/dmb2chpbx965shwigb1zbr8zwjz34sgj-nixos/nixos/lib/customisation.nix:69:16:
    
               68|     let
               69|       result = f origArgs;
                 |                ^
               70|
    
    The parameter is still named applicationName, but it’s now passed
    through as binaryName, breaking the previous parameter name detection.
    
    https://github.com/NixOS/nixpkgs/pull/165161
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- firefox-overlay.nix
+++ firefox-overlay.nix
@@ -157,7 +157,7 @@ let
         + optionalString (96 >= getMajorVersion version) (":" + makeLibraryPath [self.xorg.libXtst]);
     })) {
       ${
-        if super.firefox-unwrapped ? applicationName then
+        if super.firefox-unwrapped ? binaryName || super.firefox-unwrapped ? applicationName then
           "applicationName"
         else
           "browserName"
